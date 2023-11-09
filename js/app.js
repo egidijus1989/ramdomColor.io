ecd859822277c917082f1a433d05b60517807d54
egidijus <egidijus.navickas@stud.kitm.lt>
2023-11-09 08:28:12 +0200

    abcd

--- js/app.js
+++ js/app.js
@@ -1,50 +1,55 @@
-const container = document.querySelector('.container');
+// const container = document.querySelector('.container');
 
-for (let i = 0; i < 16; i++){
-    let row = document.createElement('div');
-    container.appendChild(row);
-    row.style.width = "400px";
-    row.style.height = "400px";
-    row.style.float = "left"
-    row.style.border = "1px solid black";
-    row.style.backgroundColor = setBg();
+// for (let i = 0; i < 16; i++){
+//     let row = document.createElement('div');
+//     container.appendChild(row);
+//     row.style.width = "400px";
+//     row.style.height = "400px";
+//     row.style.float = "left"
+//     row.style.border = "1px solid black";
+//     row.style.backgroundColor = setBg();
 
-    row.addEventListener('mouseover', ()=>{
-        row.style.backgroundColor = setBg();
-        console.log('paspaust')
-    })
-}
+//     row.addEventListener('mouseover', ()=>{
+//         row.style.backgroundColor = setBg();
+//         console.log('paspaust')
+//     })
+// }
 
-function setBg() {
-    const randomColor = Math.floor(Math.random()*16777215).toString(16);
-    return "#" + randomColor;
+// function setBg() {
+//     const randomColor = Math.floor(Math.random()*16777215).toString(16);
+//     return "#" + randomColor;
     
-  }
+//   }
 
   //------------------------------------------------------------------------------
 const photoNumber = 12;
 const photoContainer = document.querySelector('.photo-container')
-for (let i = 0; i < photoNumber; i++){
-    let img = document.createElement('img');
-    img.src=`photo/${i}.jpeg`;
-    photoContainer.appendChild(img);
-    img.addEventListener('dblclick', (e)=>{
-        img.src=`photo/20.png`;
-    })
-}
-
 const hide = document.querySelector('.show');
 hide.addEventListener('click', (e)=>{
     e.preventDefault();
-    let photoContainer = document.querySelector(".photo-container");
-    if (photoContainer.style.display === "none") {
-        photoContainer.style.display = "block";
-        console.log("rodyti");
-    } else {
-        photoContainer.style.display = "none";
-        console.log("slepti");
+    for (let i = 0; i < photoNumber; i++){
+        let img = document.createElement('img');
+        img.src=`photo/${i}.jpeg`;
+        photoContainer.appendChild(img);
+        img.addEventListener('dblclick', (e)=>{
+            img.src=`photo/20.png`;
+        })
     }
 })
+
+
+// const hide = document.querySelector('.show');
+// hide.addEventListener('click', (e)=>{
+//     e.preventDefault();
+//     let photoContainer = document.querySelector(".photo-container");
+//     if (photoContainer.style.display === "none") {
+//         photoContainer.style.display = "block";
+//         console.log("rodyti");
+//     } else {
+//         photoContainer.style.display = "none";
+//         console.log("slepti");
+//     }
+// })
 
 const shuffle = document.querySelector(".shuffle");
 shuffle.addEventListener('click', (e)=>{
